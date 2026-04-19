65d90cd6cb38f160ae09d983863287d9fd122bc0
karthik7479 <i.karthikreddy@mail.com>
2026-04-19 11:18:36 +0530

    Fix authRoutes - remove missing changePassword

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, getMe, updateProfile, changePassword } = require('../controllers/authController');
+const { register, login, getMe, updateProfile } = require('../controllers/authController');
 const { protect } = require('../middleware/auth');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/me', protect, getMe);
 router.put('/profile', protect, updateProfile);
-router.put('/change-password', protect, changePassword);
 
 module.exports = router;
